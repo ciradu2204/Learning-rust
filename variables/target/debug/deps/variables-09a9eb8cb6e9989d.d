dba0b3e3d178f32129778f0a6bf9adaf0b5c01e3
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2024-12-23 12:28:46 +0200

    added variables

---
+++ variables/target/debug/deps/variables-09a9eb8cb6e9989d.d
@@ -0,0 +1,5 @@
+/Users/cynthiairadukunda/Documents/Learning-rust/variables/target/debug/deps/variables-09a9eb8cb6e9989d: src/main.rs
+
+/Users/cynthiairadukunda/Documents/Learning-rust/variables/target/debug/deps/variables-09a9eb8cb6e9989d.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/cynthiairadukunda/Documents/Learning-rust/variables/target/debug/variables: /Users/cynthiairadukunda/Documents/Learning-rust/variables/src/main.rs
