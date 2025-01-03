17a5ae07516e13bc329c52df19bd07f65733c198
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2025-01-03 18:00:47 +0200

    updated the main.rs

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
     let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
     // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!("127.0.0.1:{}", configuration.application.port);
+    let address = format!("{}:{}", configuration.application.host,configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
